227efbf484280eda980f267a237f94c00a979ad7
Ryan Bliss <smile@ryanbliss.me>
2024-04-03 16:52:41 -0700

    resolved webpack errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { dev }) => {
+        // The condition is to have the plugin on build time, not to perturb live refresh
+        if (!dev) {
+            config.resolve.alias = {
+                'cldr$': 'cldrjs',
+                'cldr': 'cldrjs/dist/cldr'
+            };
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
