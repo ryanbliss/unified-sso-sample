299d3d3af3979389c05c68b422e1d20554b512f9
Ryan Bliss <smile@ryanbliss.me>
2024-04-13 16:57:37 -0700

    updated headers of webhook endpoints

--- src/app/api/pubsub/connect/route.ts
+++ src/app/api/pubsub/connect/route.ts
@@ -12,5 +12,13 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
   console.log("/api/pubsub/connect secret", secret);
   const body = await req.json();
   console.log("/api/pubsub/connect body", JSON.stringify(body, null, 4));
-  return NextResponse.json({});
+  return NextResponse.json(
+    {},
+    {
+      headers: {
+        "WebHook-Allowed-Origin":
+          "https://unify-sso-generic-pubsub.webpubsub.azure.com",
+      },
+    }
+  );
 }
