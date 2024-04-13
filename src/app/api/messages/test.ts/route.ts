07ea56b1a0956cbb1c5b1f4afccd0e4368155653
Ryan Bliss <smile@ryanbliss.me>
2024-04-13 14:07:59 -0700

    retry

--- src/app/api/messages/test.ts/route.ts
+++ src/app/api/messages/test.ts/route.ts
@@ -2,6 +2,7 @@ import { prepareBotPromptFiles } from "@/bot/fs-utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
+  // retry
   prepareBotPromptFiles();
   return NextResponse.json({});
 }
