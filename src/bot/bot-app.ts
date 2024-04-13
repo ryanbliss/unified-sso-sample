4dd030b1ac7c5648ed9c12147f60a908287448bc
Ryan Bliss <smile@ryanbliss.me>
2024-04-12 22:33:08 -0700

    tryin some jank

--- src/bot/bot-app.ts
+++ src/bot/bot-app.ts
@@ -100,7 +100,7 @@ const model = new OpenAIModel({
 });
 
 const prompts = new PromptManager({
-  promptsFolder: path.join(__dirname, "./src/bot/prompts"),
+  promptsFolder: path.join(import.meta.url, "./src/bot/prompts"),
 });
 
 const planner = new ActionPlanner({
