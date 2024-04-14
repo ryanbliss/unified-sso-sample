dbb2233b4b5861c05ab99ed0c4e3f021f6c8963a
Ryan Bliss <smile@ryanbliss.me>
2024-04-13 17:19:49 -0700

    fix 500 error on OPTIONS

--- src/app/api/pubsub/validate/route.ts
+++ src/app/api/pubsub/validate/route.ts
@@ -9,13 +9,6 @@ export async function OPTIONS(req: NextRequest): Promise<NextResponse> {
     "/api/pubsub/validate hub",
     req.headers.get("WebHook-Request-Origin")
   );
-  const url = new URL(req.url);
-  const hub = url.searchParams.get("event");
-  console.log("/api/pubsub/validate hub", hub);
-  const secret = url.searchParams.get("secret");
-  console.log("/api/pubsub/validate secret", secret);
-  const body = await req.json();
-  console.log("/api/pubsub/validate body", JSON.stringify(body, null, 4));
   return NextResponse.json(
     {},
     {
