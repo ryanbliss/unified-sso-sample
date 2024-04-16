bf285a93f54e3c5755fe86b6d9b69254add7269a
Ryan Bliss <smile@ryanbliss.me>
2024-04-15 18:30:53 -0700

    chain option

--- src/app/test-task-module/TestTaskModulePage.tsx
+++ src/app/test-task-module/TestTaskModulePage.tsx
@@ -1,9 +1,23 @@
 "use client";
 
+import { LoadWrapper } from "@/components/view-wrappers";
 import { Button } from "@fluentui/react-components";
 import * as teamsJs from "@microsoft/teams-js";
+import { useEffect, useState } from "react";
 
 export function TestTaskModulePage() {
+  const [loading] = useState(
+    new URL(window.location.href).searchParams.get("close") === "true"
+  );
+
+  useEffect(() => {
+    if (!loading) return;
+    teamsJs.dialog.url.submit();
+  }, [loading]);
+
+  if (loading) {
+    return <LoadWrapper text="Loading..." />;
+  }
   return (
     <>
       <Button
@@ -42,10 +56,29 @@ export function TestTaskModulePage() {
       >
         {"Finish with undefined"}
       </Button>
-      <Button onClick={() => {
-        teamsJs.dialog.url.submit();
-      }}>
+      <Button
+        onClick={() => {
+          teamsJs.dialog.url.submit();
+        }}
+      >
         {"teamsJs.dialog.url.submit()"}
+      </Button>
+      <Button
+        onClick={() => {
+          const chainUrl = new URL(`${window.location.href}?close=true`);
+          teamsJs.dialog.url.submit({
+            // Teams AI library requires the verb be attached to the data field
+            verb: "task-module",
+            response: {
+              title: "Test",
+              height: "medium",
+              width: "medium",
+              url: chainUrl,
+            },
+          });
+        }}
+      >
+        {"Chain"}
       </Button>
     </>
   );
