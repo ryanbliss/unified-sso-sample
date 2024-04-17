61d5884f4ebf2287b037bf0f49c6b1ed6171f3e2
Ryan Bliss <smile@ryanbliss.me>
2024-04-16 21:43:20 -0700

    set simulated context

--- src/context-providers/teams-client-provider/internals/teams-app-context/useTeamsAppContext.tsx
+++ src/context-providers/teams-client-provider/internals/teams-app-context/useTeamsAppContext.tsx
@@ -25,8 +25,33 @@ export const useTeamsAppContext = (
   const [error, setError] = useState<Error | undefined>();
 
   useEffect(() => {
-    if (!isTeamsJsPath()) return;
-    if (!ctx?.user?.id && initialized) {
+    if (ctx?.user?.id) return;
+    if (!isTeamsJsPath()) {
+      // When testing locally, change to light theme as a test case for changing themes from default
+      // setTheme(teamsLightTheme);
+      setCtx({
+        app: {
+          locale: "us",
+          theme: "dark",
+          sessionId: "test-session-id",
+          host: {
+            name: "Orange" as any,
+            clientType: teamsJs.HostClientType.web,
+            sessionId: "test-session-id",
+          },
+        },
+        page: {
+          id: "live-share-sandbox",
+          frameContext: teamsJs.FrameContexts.content,
+        },
+        user: {
+          id: "test",
+          userPrincipalName: "test@test.com"
+        },
+      });
+      return;
+    }
+    if (initialized) {
       console.log("useTeamsContext: Attempting to get Teams context");
       teamsJs.app
         .getContext()
