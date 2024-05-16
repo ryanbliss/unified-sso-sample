a43970d0eecfb2e870aeca7b34e139d467a18638
Ryan Bliss <smile@ryanbliss.me>
2024-05-15 18:27:56 -0700

    add loading ui

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { LoadWrapper } from "@/client/components/view-wrappers";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <LoadWrapper text="Loading..." />;
+}
