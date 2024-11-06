ce0943cee360b61bec3e330916e051a2202355a5
Ryan Bliss <smile@ryanbliss.me>
2024-11-06 14:40:28 -0800

    use bot storage values as initial react state

--- src/client/components/collab-sdk-test/internal/BotUserStorageExample.tsx
+++ src/client/components/collab-sdk-test/internal/BotUserStorageExample.tsx
@@ -5,9 +5,10 @@ import { FlexColumn, FlexRow } from "../../flex";
 import CodeBlock from "../../code-block/CodeBlock";
 
 export const BotUserStorageExample: FC = () => {
-  const [res, setRes] = useState<string>();
-  const [err, setErr] = useState<string>();
   const { client } = useTeamsClientContext();
+  const count = client?.conversation.bot.storage.user.get<number | undefined>("count") ?? 0;
+  const [res, setRes] = useState<string>(`key: count\nvalue: ${count}`);
+  const [err, setErr] = useState<string>();
 
   useEffect(() => {
     const listener = (key: string, value: any) => {
